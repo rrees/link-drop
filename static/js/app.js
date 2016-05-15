97fc34c3e08cc9168c1e53f37634697f295939a1
Robert Rees <shudderfix@gmail.com>
2016-05-15 22:55:59 +0100

    Starts to add the controller to the components

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,18 @@
 const linkDropApp = angular.module('linkDropApp', []);
 
+function QuickControlsController($scope) {
+	var ctrl = this;
+
+	$scope.collection = [];
+	
+	ctrl.showAddLink = function() {
+		return $scope.collection && $scope.collection.length > 0;
+	}
+}
+
 const ldQuickControls = {
-	templateUrl: '/static/components/controls/quick.html'
+	templateUrl: '/static/components/controls/quick.html',
+	controller: QuickControlsController
 }
 
 const ldLatestCollections = {
