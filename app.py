84f7a79a4ac81c9e7a7f0c8107bfcb84a65fd9e4
Robert Rees <shudderfix@gmail.com>
2016-05-15 22:59:59 +0100

    Creates a stub path for creating a new Collection

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ routes = [
 	('/', pages.Front()),
 	('/home', pages.Home()),
 	('/collections', links.Collections()),
+	('/collections/new', links.NewCollection()),
 	('/collection/{collection_id}', links.Collection()),
 	('/collection/{collection_id}/links', links.Links()),
 ]
