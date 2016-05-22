0e6c28380961c7cac803fe84a291fbe12692a5c8
Robert Rees <shudderfix@gmail.com>
2016-05-22 13:42:09 +0100

    Adds some datetime tracking to the models

--- models.py
+++ models.py
@@ -4,3 +4,9 @@ class Collection(ndb.Model):
 	user_id = ndb.StringProperty(required=True)
 	name = ndb.StringProperty(required=True)
 	description = ndb.StringProperty()
+	created = ndb.DateTimeProperty(auto_now_add=True)
+	updated = ndb.DateTimeProperty(auto_now=True)
+
+class Link(ndb.Model):
+	created = ndb.DateTimeProperty(auto_now_add=True)
+	updated = ndb.DateTimeProperty(auto_now=True)
