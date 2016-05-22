4c363e617c39eaf140ee5c170e885c3073a5242f
Robert Rees <shudderfix@gmail.com>
2016-05-22 13:54:15 +0100

    Makes Links be part of the Collection object

--- models.py
+++ models.py
@@ -1,12 +1,14 @@
 from google.appengine.ext import ndb
 
+class Link(ndb.Model):
+	url = ndb.StringProperty(required=True)
+	created = ndb.DateTimeProperty(auto_now_add=True)
+	updated = ndb.DateTimeProperty(auto_now=True)
+
 class Collection(ndb.Model):
 	user_id = ndb.StringProperty(required=True)
 	name = ndb.StringProperty(required=True)
 	description = ndb.StringProperty()
-	created = ndb.DateTimeProperty(auto_now_add=True)
-	updated = ndb.DateTimeProperty(auto_now=True)
-
-class Link(ndb.Model):
+	links = ndb.StructuredProperty(Link, repeated=True)
 	created = ndb.DateTimeProperty(auto_now_add=True)
 	updated = ndb.DateTimeProperty(auto_now=True)
