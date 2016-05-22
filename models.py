77e9049da30fc21d9272335138910e976ab47a06
Robert Rees <shudderfix@gmail.com>
2016-05-22 23:13:54 +0100

    Adds a flag to indicate whether a collection should be public or not and an id to use for the collection that is not related to the Key of the collection

--- models.py
+++ models.py
@@ -9,6 +9,8 @@ class Collection(ndb.Model):
 	user_id = ndb.StringProperty(required=True)
 	name = ndb.StringProperty(required=True)
 	description = ndb.StringProperty()
+	public_id = ndb.StringProperty(required=True)
+	public = ndb.BooleanProperty(required=True, default=False)
 	links = ndb.StructuredProperty(Link, repeated=True)
 	created = ndb.DateTimeProperty(auto_now_add=True)
 	updated = ndb.DateTimeProperty(auto_now=True)
